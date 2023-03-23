80ac95712c1837572c860c15f6cfac85f60d3059
Akshat Singh <91002031+Legendleo90@users.noreply.github.com>
2023-03-23 13:31:57 +0530

    Update and rename lineage_beryllium.mk to evolution_beryllium.mk

---
+++ evolution_beryllium.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2018-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/beryllium/device.mk)
+
+# Inherit some common evolution stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+EXTRA_UDFPS_ANIMATIONS := false
+IS_PHONE := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+TARGET_ENABLE_BLUR := false
+TARGET_SUPPORTS_ALERT_SLIDER := false
+
+# Maintainer Flag
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.evolution.maintainer=Akshat
+
+# Unofficial
+EVO_BUILD_MAINTAINER := Akshat
+EVO_BUILD_TYPE := UNOFFICIAL
+BUILD_USERNAME := Akshat
+BUILD_HOSTNAME := Evolution
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := evolution_beryllium
+PRODUCT_DEVICE := beryllium
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO F1
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_SYSTEM_NAME := beryllium
+
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
+    TARGET_PRODUCT="beryllium"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
