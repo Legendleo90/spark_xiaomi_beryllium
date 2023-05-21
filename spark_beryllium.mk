8de281751c5f655f754743639ed3d074edd635d6
Akshat Singh <akshatleo9456@gmail.com>
2023-05-22 04:10:18 +0530

    beryllium: Spark-ify

--- spark_beryllium.mk
+++ spark_beryllium.mk
@@ -6,8 +6,8 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common cherish stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Inherit some common spark stuff.
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
@@ -22,13 +22,11 @@ TARGET_ENABLE_BLUR := false
 
 # Maintainer Flag
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=𝔸𝕂𝕊ℍ𝔸𝕋
+    ro.spark.maintainer=𝔸𝕂𝕊ℍ𝔸𝕋
 
-# OFFICIAL
-CHERISH_BUILD_TYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := cherish_beryllium
+PRODUCT_NAME := spark_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
